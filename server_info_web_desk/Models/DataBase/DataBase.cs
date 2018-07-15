f58297e2d9ccf6e0cec43886bf4a33c73702e2ba
zsuz <Avdosevstas@mail.ru>
2018-07-15 18:19:28 +0300

    _db

--- server_info_web_desk/Models/DataBase/DataBase.cs
+++ server_info_web_desk/Models/DataBase/DataBase.cs
@@ -7,13 +7,6 @@ namespace server_info_web_desk.Models.DataBase
 {
     public static class DataBase
     {
-        //public static ApplicationDbContext db = new ApplicationDbContext();
-
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    db.Dispose();
-        //    base.Dispose(disposing);
-        //}
+       
     }
 }
